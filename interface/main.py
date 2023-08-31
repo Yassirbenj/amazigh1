774bd52fcc54cfe813a2ef66a9aa5a32823f1f90
yassir benjilali <benjilaliyassir@forleaders.net>
2023-08-31 23:17:23 +0100

    issue on data

--- interface/main.py
+++ interface/main.py
@@ -63,7 +63,7 @@ with st.form("input_form",clear_on_submit=True):
     #input_img = st.file_uploader('character image',type=['png', 'jpg','jpeg'])
     if st.form_submit_button("Predict"):
         if input_img is not None:
-            st.text(input_img)
+            st.write(np.unique(input_img))
             img = Image.fromarray(input_img)
             st.image(img)
             image=trim(img)
@@ -71,8 +71,9 @@ with st.form("input_form",clear_on_submit=True):
             st.image(new_image)
             img_array = np.array(new_image)
             img_array=img_array[:,:,:3]
+            st.write(np.unique(img_array))
             img_tensor=tf.convert_to_tensor(img_array)
-            st.text(img_tensor)
+            #st.text(img_tensor)
             loaded_model = load_model()
             prediction = predict(loaded_model,img_tensor)[0]
             pred_proba= predict(loaded_model,img_tensor)[1]
