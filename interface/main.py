121c2a05bc21de59225a9756d7717b6c28e9432f
yassir benjilali <benjilaliyassir@forleaders.net>
2023-09-01 13:14:08 +0100

    remove point

--- interface/main.py
+++ interface/main.py
@@ -32,12 +32,12 @@ def trim(image):
     return image
 
 # Specify canvas parameters in application
-drawing_mode = st.sidebar.selectbox(
-    "Drawing tool:", ("freedraw"))
+#drawing_mode = st.sidebar.selectbox(
+   # "Drawing tool:", ("freedraw"))
 
-stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
-if drawing_mode == 'point':
-    point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
+#stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
+#if drawing_mode == 'point':
+   # point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
 #stroke_color = st.sidebar.color_picker("Stroke color hex: ")
 #bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
 #bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
@@ -48,14 +48,14 @@ with st.form("input_form",clear_on_submit=True):
     st.write("<h3>Upload your image for the magic ✨</h3>", unsafe_allow_html=True)
     canvas_result = st_canvas(
                     fill_color="rgba(255, 255, 255, 0.3)",  # Fixed fill color with some opacity
-                    stroke_width=stroke_width,
+                    #stroke_width=stroke_width,
                     #stroke_color=stroke_color,
                     #background_color=bg_color,
                     #background_image=Image.open(bg_image) if bg_image else None,
                     update_streamlit=realtime_update,
                     height=150,
                     drawing_mode="freedraw",
-                    point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
+                    #point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
                     key="canvas",
                     )
     input_img=canvas_result.image_data
